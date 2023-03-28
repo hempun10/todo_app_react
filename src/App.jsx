38b7f6cc294a65070837c745f727ba104734c3d3
Hem Pun <hemthegentlmen@gmail.com>
2023-03-28 19:45:31 +0545

    Done with UI, onclick Event, adding items, deleting items

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react'
 import Todo from './components/Todo'
+import './Style.css'
 
 function App() {
   const [count, setCount] = useState(0)
--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,11 +1,72 @@
-import React from 'react';
-
+import React, { useState } from "react";
+import "../Style.css";
 const Todo = () => {
-  return (
-    <div>
-      
-    </div>
-  );
+  const [inputData, setInputData] = useState("");
+  const [item,setItem] = useState([])
+
+//   Add all items
+const addItem =()=>{
+    if(!inputData){
+        alert("please enter a todo item")
+    }else{
+        const myinputData = {
+            id : new Date().getTime().toString(),
+            name: inputData
+        }
+        setItem([...item , myinputData])
+        setInputData('')
+    }
 }
+// Delete each item
+const deleteItem =(index)=>{
+    const updatedItem = item.filter((currElem)=>{
+        return currElem.id !== index;
+    })
+    setItem(updatedItem)
+}
+  return (
+    <>
+      <div className="main-div">
+        <div className="child-div">
+          <figure>
+            <img src="../../assets/todo.svg" alt="todo logo" />
+            <figcaption>Add your todo List Here 🫶</figcaption>
+          </figure>
+          <div className="addItems">
+            <input
+              type="text"
+              placeholder="Add Item ✍️"
+              className="form-control"
+              value={inputData}
+              onChange={(e) => setInputData(e.target.value)}
+            />
+            <i class="fa fa-solid fa-plus" onClick={addItem}></i>
+          </div>
+          {/* Show our items */}
+          <div className="showItems">
+          {item.map((currElem)=>{
+            const { id,name }= currElem
+            return(
+            <div className="eachItem" key={id}>
+              <h3>{name}</h3>
+              <div className="todo-btn">
+                <i class="far fa-solid fa-edit"></i>
+                <i class="far fa-solid fa-trash-alt" onClick={()=> deleteItem(id)}></i>
+              </div>
+            </div>
+            )
+          })}
+          </div>
+          <div className="showItems">
+            {/* remove all items */}
+            <button className="btn effect04" data-sm-link-text="Remove All" onClick={()=> setItem([])}>
+              <span>CHECKLIST</span>{" "}
+            </button>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
 
 export default Todo;
